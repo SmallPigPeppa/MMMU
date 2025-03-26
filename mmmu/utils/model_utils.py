b575aae412b829d8c9f5e198d91a146e6d92c881
lwz <905328134@qq.com>
2025-03-26 17:13:34 +0800

    initial

--- mmmu/utils/model_utils.py
+++ mmmu/utils/model_utils.py
@@ -53,12 +53,13 @@ def call_llava_engine_df(args, sample, model, tokenizer=None, processor=None):
             max_new_tokens=128,
             use_cache=True)
 
-        input_token_len = input_ids.shape[1]
-        import pdb; pdb.set_trace()
-        n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
-        if n_diff_input_output > 0:
-            print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
-        response = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+        # input_token_len = input_ids.shape[1]
+        # import pdb; pdb.set_trace()
+        # n_diff_input_output = (input_ids != output_ids[:, :input_token_len]).sum().item()
+        # if n_diff_input_output > 0:
+        #     print(f'[Warning] {n_diff_input_output} output_ids are not the same as the input_ids')
+        # response = tokenizer.batch_decode(output_ids[:, input_token_len:], skip_special_tokens=True)[0]
+        response = tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
     else:  # multiple images actually
         if sample['question_type'] == 'multiple-choice':
             all_choices = sample['all_choices']
