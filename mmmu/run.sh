5a9ab5f0bb11607cf2e7c37e10f91e62e6c3c05d
lwz <905328134@qq.com>
2025-03-27 18:14:38 +0800

    initial

--- mmmu/run.sh
+++ mmmu/run.sh
@@ -22,8 +22,16 @@
 #--config_path configs/llava1.5.yaml
 
 #/ppio_net0/code/LLaVA/continual-ckpt/domain-merged/llava-v1.5-7b-lora-task-vg-merged
+#export HF_HOME=/ppio_net0/huggingface
+#CUDA_VISIBLE_DEVICES=0 python run_llava.py \
+#--output_path example_outputs/llava-v1.5-7b-ft-val.json \
+#--model_path /ppio_net0/code/LLaVA/continual-ckpt/domain-merged/llava-v1.5-7b-lora-task-vg-merged \
+#--config_path configs/llava1.5.yaml
+
+
+#/ppio_net0/code/LLaVA/finetune-ckpt/lwf-pretrain-lambda0.2/llava-v1.5-7b-lora-task6-vg-merged
 export HF_HOME=/ppio_net0/huggingface
 CUDA_VISIBLE_DEVICES=0 python run_llava.py \
---output_path example_outputs/llava-v1.5-7b-ft-val.json \
---model_path /ppio_net0/code/LLaVA/continual-ckpt/domain-merged/llava-v1.5-7b-lora-task-vg-merged \
+--output_path example_outputs/llava-v1.5-7b-lwf-val.json \
+--model_path /ppio_net0/code/LLaVA/finetune-ckpt/lwf-pretrain-lambda0.2/llava-v1.5-7b-lora-task6-vg-merged \
 --config_path configs/llava1.5.yaml
