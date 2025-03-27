760f9e0351cb911df2c4bbe334db419e065ab672
lwz <905328134@qq.com>
2025-03-27 16:22:33 +0800

    initial

--- mmmu/run.sh
+++ mmmu/run.sh
@@ -8,6 +8,6 @@
 #/ppio_net0/code/LLaVA/checkpoints/llava-v1.5-7b-lora-merged
 export HF_HOME=/ppio_net0/huggingface
 CUDA_VISIBLE_DEVICES=0 python run_llava.py \
---output_path example_outputsllava-v1.5-7b-lora-val.json \
+--output_path example_outputs/llava-v1.5-7b-lora-val.json \
 --model_path /ppio_net0/code/LLaVA/checkpoints/llava-v1.5-7b-lora-merged \
 --config_path configs/llava1.5.yaml
