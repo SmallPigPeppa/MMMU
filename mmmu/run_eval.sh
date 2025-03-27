ca8739e6da87a78dc8ec4a42f1c8671034fdef19
lwz <905328134@qq.com>
2025-03-27 17:09:02 +0800

    initial

--- mmmu/run_eval.sh
+++ mmmu/run_eval.sh
@@ -1,6 +1,7 @@
 
 #python main_eval_only.py --output_path example_outputs/llava1.5_7b_val.json
-python main_eval_only.py --output_path example_outputs/llava-v1.5-7b-lora-val.json
+#python main_eval_only.py --output_path example_outputs/llava-v1.5-7b-lora-val.json
+python main_eval_only.py --output_path example_outputs/llava-v1.5-7b-c-val.json
 
 #python main_parse_and_eval.py --path ./example_outputs/llava1.5_7b_val.json --subject ALL # all subject
 #
