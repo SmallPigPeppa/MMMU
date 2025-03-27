d9f6934e318967fbf656bb72ad552f2a2632e37f
lwz <905328134@qq.com>
2025-03-27 17:19:04 +0800

    initial

--- mmmu/run.sh
+++ mmmu/run.sh
@@ -15,8 +15,15 @@
 
 
 #/ppio_net0/code/LLaVA/continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-vg-merged
+#export HF_HOME=/ppio_net0/huggingface
+#CUDA_VISIBLE_DEVICES=0 python run_llava.py \
+#--output_path example_outputs/llava-v1.5-7b-c-val.json \
+#--model_path /ppio_net0/code/LLaVA/continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-vg-merged \
+#--config_path configs/llava1.5.yaml
+
+#/ppio_net0/code/LLaVA/continual-ckpt/domain-merged/llava-v1.5-7b-lora-task-vg-merged
 export HF_HOME=/ppio_net0/huggingface
 CUDA_VISIBLE_DEVICES=0 python run_llava.py \
---output_path example_outputs/llava-v1.5-7b-c-val.json \
---model_path /ppio_net0/code/LLaVA/continual-ckpt/llava-c/llava-v1.5c-7b-lora-task-vg-merged \
+--output_path example_outputs/llava-v1.5-7b-ft-val.json \
+--model_path /ppio_net0/code/LLaVA/continual-ckpt/domain-merged/llava-v1.5-7b-lora-task-vg-merged \
 --config_path configs/llava1.5.yaml
